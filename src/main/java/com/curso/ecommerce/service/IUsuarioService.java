c261592b247723a50cabdcfce25afd5cda4de70b
Jhon1172001 <jhonespinozah117@gmail.com>
2024-10-19 01:01:16 -0500

    obtener usuario login y session

--- src/main/java/com/curso/ecommerce/service/IUsuarioService.java
+++ src/main/java/com/curso/ecommerce/service/IUsuarioService.java
@@ -6,7 +6,7 @@ import com.curso.ecommerce.model.Usuario;
 
 public interface IUsuarioService {
 	Optional<Usuario> findById(Integer id);
-
 	Usuario save(Usuario usuario);
+	Optional<Usuario> findByEmail(String email);
 
 }
