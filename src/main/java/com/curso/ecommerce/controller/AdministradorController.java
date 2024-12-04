11d161f090c34c62f67ab5a3585299c7c5d88633
Jhon1172001 <jhonespinozah117@gmail.com>
2024-12-04 00:54:33 -0500

    Revert "final exa"
    This reverts commit 5c34b96490c25d3fb7fcd6004f1b9214cacbc888.

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -2,16 +2,12 @@ package com.curso.ecommerce.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.IOrdenService;
 import com.curso.ecommerce.service.IUsuarioService;
@@ -29,8 +25,6 @@ public class AdministradorController {
 	
 	@Autowired
 	private IOrdenService ordensService;
-	
-	private Logger logg= LoggerFactory.getLogger(AdministradorController.class);
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -51,15 +45,5 @@ public class AdministradorController {
 	public String ordenes(Model model) {
 		model.addAttribute("ordenes", ordensService.findAll());
 		return "administrador/ordenes";
-	}
-	
-	@GetMapping("/detalle/{id}")
-	public String detalle(Model model, @PathVariable Integer id) {
-		logg.info("Id de la orden {}",id);
-		Orden orden= ordensService.findById(id).get();
-		
-		model.addAttribute("detalles", orden.getDetalle());
-		
-		return "administrador/detalleorden";
 	}
 }
